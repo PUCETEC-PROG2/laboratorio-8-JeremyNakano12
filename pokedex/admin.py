6fc96ecbc1e11aac82b70fa81583ea3eda5c8352
JeremyNakano <jeremarinx@gmail.com>
2024-07-08 16:57:33 -0500

    Terminado

--- pokedex/admin.py
+++ pokedex/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from .models import Pokemon
+from .models import Pokemon, Trainer
 
 # Register your models here.
 @admin.register(Pokemon)
 class PokemnAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Trainer)
+class TrainerAdmin(admin.ModelAdmin):
     pass
--- pokedex/models.py
+++ pokedex/models.py
@@ -10,3 +10,12 @@ class Pokemon (models.Model):
     
     def __str__(self) -> str:
         return self.name
+    
+class Trainer (models.Model):
+    name = models.CharField(max_length=30, null=False)
+    age = models.IntegerField(null=False)
+    level = models.IntegerField(null=False)
+    region = models.CharField(max_length=30, null=True)
+    
+    def __str__(self) -> str:
+        return self.name
